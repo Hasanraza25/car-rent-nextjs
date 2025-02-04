011684e807051d430932ea4f2052ac33b0d3e2cd
Hasanraza25 <hasansaeed2526@gmail.com>
2025-02-04 10:10:04 -0700

    change country

--- src/app/components/PickUpDropOff/PickUpDropOff.jsx
+++ src/app/components/PickUpDropOff/PickUpDropOff.jsx
@@ -25,7 +25,7 @@ const PickUpDropOff = () => {
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ country: "Pakistan" }),
+          body: JSON.stringify({ country: "United States" }),
         }
       );
       const data = await response.json();
