8089bedbae374c38bd0d638ffe09bd93ab3824a6
Hasanraza25 <hasansaeed2526@gmail.com>
2025-02-22 18:51:05 +0500

    changes

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,5 @@
-import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const adminEmail = process.env.ADMIN_EMAIL;
 const adminPassword = process.env.ADMIN_PASSWORD;
@@ -7,36 +7,34 @@ const adminPassword = process.env.ADMIN_PASSWORD;
 const authOptions = {
   providers: [
     CredentialsProvider({
-      name: 'Credentials',
+      name: "Credentials",
       credentials: {
-        email: { label: 'Email', type: 'email' },
-        password: { label: 'Password', type: 'password' },
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
       },
       authorize: async (credentials) => {
         const { email, password } = credentials;
 
-        // Check if the provided credentials match the hardcoded admin credentials
         if (email === adminEmail && password === adminPassword) {
           return { email };
         } else {
-          throw new Error('Invalid credentials');
+          throw new Error("Invalid credentials");
         }
       },
     }),
   ],
   session: {
-    strategy: 'jwt',
+    strategy: "jwt",
   },
   jwt: {
     secret: process.env.JWT_SECRET,
   },
   pages: {
-    signIn: '/admin/login',
-    signOut: '/',
-    error: '/admin/login', // Redirect to login page on error
+    signIn: "/admin/login",
+    signOut: "/",
+    error: "/admin/login",
   },
 };
 
-// Export named handlers for each HTTP method
 export const GET = (req, res) => NextAuth(req, res, authOptions);
 export const POST = (req, res) => NextAuth(req, res, authOptions);
